2455485a3cb1cbc14ce105d21f2308b0ada36816
omnip620 <omnip@OmniPs-MacBook-Pro.local>
2019-03-25 22:55:06 -0400

    feat: add all

---
+++ index.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+const ses = new AWS.SES();
+const dynamoDB = new AWS.DynamoDB();
+
+const DOMAIN_NAME = process.env.DOMAIN_NAME;
+
+exports.handler = async (event, context) => {
+  const email = event.Records[0].Sns.Message;
+
+  const param = {
+    TableName: "csye6225",
+    Key: {
+      id: { S: email }
+    }
+  };
+
+  const item = await dynamoDB.getItem(param).promise();
+  if (item == null) {
+    dynamoDB.putItem({
+      TableName: "csye6225",
+      Item: {
+        id: { S: email },
+        token: { S: context.awsRequestId },
+        ttl: { N: (Math.floor(Date.now() / 1000) + 60).toString() }
+      }
+    });
+  }
+
+  const token = item.token.S || context.awsRequestId;
+
+  const body = `http://${DOMAIN_NAME}/reset?email=${email}&token=${token}`;
+
+  const emailObj = {
+    Destination: {
+      ToAddresses: [email]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: body
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Reset password"
+      }
+    },
+    Source: `reset-password@${DOMAIN_NAME}`
+  };
+
+  ses.sendEmail(emailObj, () => {});
+};
