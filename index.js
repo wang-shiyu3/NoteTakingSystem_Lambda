33e209622d4b6822d1df61b9dabdbb78e2ff63b1
zhichao pan <pan.z@husky.neu.edu>
2019-03-27 03:51:19 -0400

    feat: update lambda

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ exports.handler = async (event, context) => {
 
   console.log(item);
 
-  const token = item.Item.token.S || context.awsRequestId;
+  const token = item.Item && item.Item.token.S || context.awsRequestId;
 
   const body = `http://${DOMAIN_NAME}/reset?email=${email}&token=${token}`;
 
